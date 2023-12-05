1b9b56715394796899a3839300d54ee667fe98c3
Ahmed Gamal <victorylover987@gmail.com>
2023-12-05 18:17:53 -0500

    added base url

--- middleware/google_auth.js
+++ middleware/google_auth.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/auth/google/callback",
+      callbackURL: `${process.env.BASE_URL}/auth/google/callback`,
       passReqToCallback: true,
     },
     async (request, accessToken, refreshToken, profile, done) => {
