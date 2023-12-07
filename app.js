c0cefc4d67d0dc4bfd846b2f8d1e4fda9884cf63
Ahmed Gamal <victorylover987@gmail.com>
2023-12-07 10:25:02 -0500

    added the route to the app.js file

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var usersRouter = require("./routes/users");
 var cartRouter = require("./routes/carts");
 var booklistRouter = require("./routes/booklists");
 var transactionRouter = require("./routes/transactions");
+var adminRouter = require("./routes/transactions");
 
 app.use(
   session({
@@ -33,6 +34,7 @@ app.use("/auth", usersRouter);
 app.use("/cart", cartRouter);
 app.use("/booklist", booklistRouter);
 app.use("/transaction", transactionRouter);
+app.use("/admin", adminRouter);
 
 let db_conn_url = null;
 
